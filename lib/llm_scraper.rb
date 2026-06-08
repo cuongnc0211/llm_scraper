607efad10a82d38247ecd62de7d1365016fd498f
cuongnc0211 <cuongnguyenfu@gmail.com>
2026-06-08 10:14:06 +0700

    init

---
+++ lib/llm_scraper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "llm_scraper/version"
+
+module LlmScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/llm_scraper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LlmScraper do
+  it "has a version number" do
+    expect(LlmScraper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
